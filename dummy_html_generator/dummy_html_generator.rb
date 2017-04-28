bb639b3bdbb4f1023334e35dd833ec53c2e7fcbf
wtetsu <tewatana@gmail.com>
2017-04-29 01:13:56 +0900

    Add dummy_html_generator

---
+++ dummy_html_generator/dummy_html_generator.rb
@@ -0,0 +1,17 @@
+﻿require "Find"
+require "FileUtils"
+
+template = File.read("template.html")
+
+Find.find("in") {|path|
+  next if !path.end_with?(".html")
+  rpath = path.sub("in/", "")
+  out_path = "out/#{rpath}"
+  out_dir_path = File.dirname(out_path)
+  FileUtils.mkdir_p(out_dir_path)
+  
+  new_link = "http://newsite.com/#{rpath}"
+  new_html = template.sub("$LINK$", new_link)
+  File.write(out_path, new_html)
+  puts out_path
+}
