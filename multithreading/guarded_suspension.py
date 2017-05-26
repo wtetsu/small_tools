29c92bff3798653009a93582c23ee136c517653e
wtetsu <tewatana@gmail.com>
2017-05-26 22:39:09 +0900

    Add comments

--- multithreading/guarded_suspension.py
+++ multithreading/guarded_suspension.py
@@ -1,3 +1,6 @@
+"""This is a implementation of multithreading design pattern Guarded Suspension."""
+# -*- coding: utf-8 -*-
+
 import thread
 import threading
 import time
@@ -5,13 +8,15 @@ import collections
 import random
 
 class RequestQueue:
+    """Thread safe Queue class which is accessed from multi threads."""
     def __init__(self):
         self.queue = collections.deque()
         self.wait_lock = threading.Lock()
         self.get_lock = threading.Lock()
         self.put_lock = threading.Lock()
-    
+
     def get_request(self):
+        """Get a request from the internal queue."""
         with self.get_lock:
             r = None
             while True:
@@ -21,14 +26,21 @@ class RequestQueue:
                     r = self.queue.popleft()
                     break
         return r
-        
+
     def put_request(self, request):
+        """Put a request to the internal queue."""
         with self.put_lock:
             self.queue.append(request)
             if self.wait_lock.locked():
                 self.wait_lock.release()
 
 def server(requestQueue, threadname):
+    """Function as server thread.
+    Puts requests to a requestQueue endlessly.
+    # Arguments
+        requestQueue:
+        threadname: thread name and it'll be printed.
+    """
     i = 0
     while True:
         requestQueue.put_request(threadname + " > message" + str(i))
@@ -36,6 +48,12 @@ def server(requestQueue, threadname):
         i += 1
 
 def client(requestQueue, threadname):
+    """Function as client thread.
+    Gets requests from a requestQueue endlessly.
+    # Arguments
+        requestQueue:
+        threadname: thread name and it'll be printed.
+    """
     while True:
         r = requestQueue.get_request()
         print(r + ":" + " > " + threadname)
