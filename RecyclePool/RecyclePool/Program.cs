fda21f3e627cc410c58794bd3fa0578e827146f1
wtetsu <tewatana@gmail.com>
2017-08-02 17:17:53 +0900

    Add RecyclePool

---
+++ RecyclePool/RecyclePool/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Web.Administration;
+using System;
+
+namespace RecyclePool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var serverManager = new ServerManager())
+            {
+                foreach (Site site in serverManager.Sites)
+                {
+                    string poolName = site.Applications["/"].ApplicationPoolName;
+                    ApplicationPool pool = serverManager.ApplicationPools[poolName];
+                    pool.Recycle();
+                    Console.WriteLine(site.Name);
+                }
+            }
+        }
+    }
+}
